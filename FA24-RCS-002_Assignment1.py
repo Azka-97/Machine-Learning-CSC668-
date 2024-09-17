e44ed6f6be195eee15a69ec097f5e5ebeb2b99c9
Azka-97 <fa24-rcs-002@cuilahore.edu.pk>
2024-09-17 03:44:51 -0700

    Update FA24-RCS-002_Assignment1.py

--- FA24-RCS-002_Assignment1.py
+++ FA24-RCS-002_Assignment1.py
@@ -181,7 +181,7 @@ print(df)
 
 #Question-4.6
 print("\nQuestion-4.6")
-df = pd.read_csv(r'E:\FA24-MS\machine learning\fa24-rcs-002_assignment1\hello_sample.csv')
+df = pd.read_csv(r'hello_sample.csv')
 
 # Question-4.7
 print("\nQuestion-4.7")
